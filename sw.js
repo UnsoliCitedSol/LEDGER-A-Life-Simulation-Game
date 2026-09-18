901f14f2c908a989defff4ed3451da17f493fa6f
UnsoliCitedSol <gunawanzander@gmail.com>
2026-09-18 15:46:32 +0800

    Add service worker to enable game installation

---
+++ sw.js
@@ -0,0 +1,4 @@
+// This forces the browser to allow the game to be installed
+self.addEventListener('fetch', (event) => {
+  // Leave this empty! It satisfies the browser requirement without breaking your game.
+});
